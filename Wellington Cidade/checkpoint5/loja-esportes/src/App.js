f8d475cc7bb52b93c2d40280b7078025a2829ccd
anadantasp <caroldantasp@gmail.com>
2023-04-26 09:14:17 -0300

    Adicionando componente footer

--- Wellington Cidade/checkpoint5/loja-esportes/src/App.js
+++ Wellington Cidade/checkpoint5/loja-esportes/src/App.js
@@ -1,10 +1,12 @@
 import Home from './componentes/Home'
 import Navbar from './componentes/Navbar'
+import Footer from './componentes/Footer'
 function App() {
   return (
     <div>
       <Navbar/>
       <Home/>
+      <Footer/>
     </div>
   );
 }
