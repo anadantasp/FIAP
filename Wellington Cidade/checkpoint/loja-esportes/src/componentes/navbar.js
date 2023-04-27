f60fc00bccf74ec816dd178decbf7ec2b7dd0320
anadantasp <caroldantasp@gmail.com>
2023-04-27 17:12:59 -0300

    criando o componente navbar

---
+++ Wellington Cidade/checkpoint/loja-esportes/src/componentes/navbar.js
@@ -0,0 +1,18 @@
+import './css/navbar.css'
+
+function Nav(){
+    return(
+        <div>
+            <ul>
+                <li>
+                    HOME
+                </li>
+                <li>
+                    FALE CONOSCO
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav
