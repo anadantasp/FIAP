b57de306e5179408cfeaecb7f71d4d9d35c3aaba
anadantasp <caroldantasp@gmail.com>
2023-04-27 20:40:42 -0300

    Adicionando usestate ao projeto

--- Wellington Cidade/checkpoint/loja-esportes/src/App.js
+++ Wellington Cidade/checkpoint/loja-esportes/src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/contato" element={<Contato/>}/>
         </Routes>
-        <Footer/>
+       <Footer/>
     </BrowserRouter>
     </>
   );
--- Wellington Cidade/checkpoint/loja-esportes/src/componentes/contato.js
+++ Wellington Cidade/checkpoint/loja-esportes/src/componentes/contato.js
@@ -1,11 +1,41 @@
+import React,{useState} from 'react'
+import './css/contato.css'
+
 function Contato(){
+
+    const[nome,setNome]= useState ("")
+    const[email,setEmail] = useState("")
+    const[userEmail,setUserEmail] = useState()
+
+    function enviarEmail(e){
+
+        e.preventDefault()
+        setUserEmail(email)
+        console.log(userEmail)
+    }
+
+    function limparEmail(){
+        setUserEmail('')
+    }
+
     return(
-        <div>
+        <div className='contato'>
             <h3>Preencha o formulário abaixo</h3>
-            <p>Nome: <input type="text"/></p>
-            <p>E-mail:<input type="text"/></p>
+            <p>Nome: <input type="text" onChange={(e)=>setNome(e.target.value)}/></p>
+            <p>E-mail:<input type="text" onChange={(e)=>setEmail(e.target.value)}/></p>
             <p>Mensagem: <textarea rows="4" cols="50"></textarea></p>
+            <button type="submit" onClick={enviarEmail}>Enviar</button>
+
+            {userEmail &&(
+            <div  className='condicional'>
+                <p>O email do usuário é: <span>{userEmail}</span></p>
+            </div>
+
+         )}
+            
         </div>
+        
+        
     )
 }
 
