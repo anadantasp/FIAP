8beee71ee1f028cf22b13fb01fe6866266621628
anadantasp <caroldantasp@gmail.com>
2023-04-27 17:25:37 -0300

    criando o componente home

--- Wellington Cidade/checkpoint/loja-esportes/src/App.js
+++ Wellington Cidade/checkpoint/loja-esportes/src/App.js
@@ -1,11 +1,14 @@
 import Navbar from './componentes/navbar'
 import Footer from './componentes/footer'
+import Home from './componentes/home'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Home/>
       <Footer/>
+      
     </>
   );
 }
---
+++ Wellington Cidade/checkpoint/loja-esportes/src/componentes/home.js
@@ -0,0 +1,39 @@
+import './css/home.css'
+
+function Home(){
+
+    return(
+        <>
+            <h3>Produtos mais vendidos</h3>
+            <section>
+                <article>
+                    <img src="https://decathlonpro.vtexassets.com/arquivos/ids/2763414-588-752/segunda-pele-simple-warm-feminina-wedze-preto-m2.jpg?v=637544696247830000" alt='imagem'></img>
+                </article>
+                <article>
+                    <img src="https://decathlonpro.vtexassets.com/arquivos/ids/2763414-588-752/segunda-pele-simple-warm-feminina-wedze-preto-m2.jpg?v=637544696247830000" alt='imagem'></img>
+                </article>
+                <article>
+                    <img src="https://decathlonpro.vtexassets.com/arquivos/ids/2763414-588-752/segunda-pele-simple-warm-feminina-wedze-preto-m2.jpg?v=637544696247830000" alt='imagem'></img>
+                </article>
+                <article>
+                    <img src="https://decathlonpro.vtexassets.com/arquivos/ids/2763414-588-752/segunda-pele-simple-warm-feminina-wedze-preto-m2.jpg?v=637544696247830000" alt='imagem'></img>
+                </article>
+                <article>
+                    <img src="https://decathlonpro.vtexassets.com/arquivos/ids/2763414-588-752/segunda-pele-simple-warm-feminina-wedze-preto-m2.jpg?v=637544696247830000" alt='imagem'></img>
+                </article>
+                <article>
+                    <img src="https://decathlonpro.vtexassets.com/arquivos/ids/2763414-588-752/segunda-pele-simple-warm-feminina-wedze-preto-m2.jpg?v=637544696247830000" alt='imagem'></img>
+                </article>
+                <article>
+                    <img src="https://decathlonpro.vtexassets.com/arquivos/ids/2763414-588-752/segunda-pele-simple-warm-feminina-wedze-preto-m2.jpg?v=637544696247830000" alt='imagem'></img>
+                </article>
+                <article>
+                    <img src="https://decathlonpro.vtexassets.com/arquivos/ids/2763414-588-752/segunda-pele-simple-warm-feminina-wedze-preto-m2.jpg?v=637544696247830000" alt='imagem'></img>
+                </article>
+
+            </section>
+        </>
+    )
+}
+
+export default Home
